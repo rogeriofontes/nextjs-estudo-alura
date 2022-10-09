36b0f6317738c8039e926b5bc459193255b8aad1
Rogério Fontes <fontestz@gmail.com>
2022-10-09 08:44:59 -0300

    acerto no build app

--- pages/README.md
+++
@@ -1,7 +0,0 @@
-# yarn build && yarn start
-
-"export": "next build && next export",
-
-# yarn export
-
-# npx http-server ./out -c-1
--- pages/_app.js
+++ pages/_app.js
@@ -16,14 +16,14 @@ function MyApp({ Component, pageProps }) {
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
 
-                            gtag('config', 'UA-455646839-2');
+                            gtag('config', 'UA-45564683-2');
                         `
                     }}
                 />
 
 
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
             </Head>
             <Script src="https://polyfill.io/v3/polyfill.min.js" strategy="beforeInteractive" />
